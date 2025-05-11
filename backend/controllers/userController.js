78cb571a05133b5a54bdfd0586c3fa0b25fc800f
nodeSkin <shittujamiu@gmail.com>
2025-05-11 09:04:26 +0100

    signed up mongoDB account, registered and save a user on the database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,44 +1,67 @@
-import asyncHandler from 'express-async-handler';
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 //@desc Auth user/set token
 // routes POST /api/users/auth
 // @access Public
 
 const authUser = asyncHandler(async (req, res) => {
-res.status(200).json({message: "Auth User"});
-})
+  res.status(200).json({ message: "Auth User" });
+});
 
 //@desc Register user
 // routes POST /api/users/register
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-res.status(200).json({message: "Register User"});
-})
+    const { name, email, password } = req.body;
+    console.log(name)
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+});
 
 //@desc Logout user
 // routes POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-res.status(200).json({message: "Logout User"});
-})
+  res.status(200).json({ message: "Logout User" });
+});
 
 //@desc Get user profile
 // routes GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-res.status(200).json({message: " Get User Profile"});
-})
+  res.status(200).json({ message: " Get User Profile" });
+});
 
 //@desc Update user profile
 // routes PUT /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-res.status(200).json({message: "Update User Profile"});
-})
+  res.status(200).json({ message: "Update User Profile" });
+});
 
 export {
-    authUser,
-    registerUser,
-    logoutUser,
-    getUserProfile,
-    updateUserProfile
-}
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes.js";
-import {notFound, errorHandler} from "./middleware/errorMiddleware.js";
-
 dotenv.config();
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+const Port = process.env.PORT || 4000;
+
+connectDB();
+
 // Importing express and dotenv to manage environment variables
 const app = express();
-const Port = process.env.PORT || 4000;
+app.use(express.json()); // Middleware to parse JSON requests
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded requests
 
 app.use("/api/users", userRoutes);
 
